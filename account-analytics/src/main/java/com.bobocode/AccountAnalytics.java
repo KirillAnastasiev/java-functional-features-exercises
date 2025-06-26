8f8bd95d70905389140604009b8cc9f324d36588
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-06-26 15:19:18 +0300

    account-analytics: AccountAnalytics.findAccountsByBirthdayMonth() done

--- account-analytics/src/main/java/com.bobocode/AccountAnalytics.java
+++ account-analytics/src/main/java/com.bobocode/AccountAnalytics.java
@@ -7,8 +7,7 @@ import java.math.BigDecimal;
 import java.time.Month;
 import java.util.*;
 
-import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 
 /**
  * Implement methods using Stream API
@@ -41,7 +40,9 @@ public class AccountAnalytics {
      * @return a list of accounts
      */
     public List<Account> findAccountsByBirthdayMonth(Month birthdayMonth) {
-        throw new UnsupportedOperationException("It's your job to implement this method"); // todo
+        return accounts.stream()
+                       .filter(a -> a.getBirthday().getMonth() == birthdayMonth)
+                       .collect(toUnmodifiableList());
     }
 
     /**
