f021c5118d9b53372ac781e3a69f4c6264bded8c
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-06-26 18:11:57 +0300

    crazy-lambdas: CrazyLambdas.isEmptyPredicate() done

--- crazy-lambdas/src/main/java/com/bobocode/CrazyLambdas.java
+++ crazy-lambdas/src/main/java/com/bobocode/CrazyLambdas.java
@@ -22,7 +22,7 @@ public class CrazyLambdas {
      * @return a string predicate
      */
     public static Predicate<String> isEmptyPredicate() {
-        throw new UnsupportedOperationException("It's your job to implement this method"); // todo
+        return String::isEmpty;
     }
 
     /**
